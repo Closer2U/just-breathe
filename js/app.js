05f38bf826b70e69a0ef9a3d1fe565d7878a0915
Closer2U <Signmeup@null.net>
2024-02-21 22:15:17 +0100

    FIX on start() jump in animation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log('I am running!');
+
+function start(){
+    var startBtn = document.getElementById("Start");
+    var pulse = document.getElementById("Pulse");
+    startBtn.style.display = "none";
+    pulse.style.animation =  "pulse 9s infinite";
+
+    //timer funktion aufrufen
+
+}
+
+//timer Logik  hier ()
+
+//ende timer überwachen
