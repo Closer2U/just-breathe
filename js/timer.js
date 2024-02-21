3cea5adc6008a251ebb703640469ce8cfeecec9b
Closer2U <Signmeup@null.net>
2024-02-22 00:03:47 +0100

    BUG show timer not working

--- js/timer.js
+++ js/timer.js
@@ -8,22 +8,55 @@ function startTimer(duration, display) {
         seconds = seconds < 10 ? "0" + seconds : seconds;
 
         display.textContent = minutes + ":" + seconds;
+        
+        fadeTimer();
 
         if (--timer < 0) {
             timer = duration;
-        
 
+    // Put all CSS back to First Iteration
         clearInterval(interval);
-
+        document.getElementById("Pulse").style.animation = "";
+        document.getElementById("Start").style.display = "";
         }
     }, 1000);
-
 }
 
 window.onclick = function () {
-    var oneMinute = 60,
-        display = document.getElementById('Timer');
+    display = document.getElementById('Timer');
+    var oneMinute = 5 ; //,
+    // display = document.getElementById('Timer');
     startTimer(oneMinute, display);
-
-
 };
+
+
+var display = document.getElementById('Timer');
+
+function fadeTimer()  {
+        console.log("Display: ")
+        var fadeEffect = setInterval(function () {
+            if (!display.style.opacity) {
+                display.style.opacity = 1;
+            }
+            if (display.style.opacity > 0) {
+                display.style.opacity -= 0.05;
+            } else {
+                clearInterval(fadeEffect);
+            }
+        }, 100);
+    }
+
+display.onclick = function showTimer ()  {
+        console.log("Display2: ")
+        //TODO fix damit man den Timer wieder einblenden kann
+        var showEffect = setInterval(function () {
+            if (!display.style.opacity) {
+                display.style.opacity = 1;
+            }
+            if (display.style.opacity < 1) {
+                display.style.opacity += 0.5;
+            } else {
+                clearInterval(showEffect);
+            }
+        }, 100);
+    }
