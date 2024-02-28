b430502f346e662d81c094474bdf83e118a08893
Ketyra Sai <Signmeup@null.net>
2024-02-28 20:42:49 +0100

    Update timer.js - auskommentiertes aus code gelöscht

--- js/timer.js
+++ js/timer.js
@@ -1,6 +1,5 @@
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
-    
     var interval = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
@@ -23,43 +22,8 @@ function startTimer(duration, display) {
 fade();
 }
 
-//window.onload = function () {
-    // document.getElementById("Pulse").onclick = function () {
-    function startMinute() {
+function startMinute() {
     var oneMinute = 60,
         display = document.getElementById('Timer');
     startTimer(oneMinute, display);
 };
-
-
-
-/* *function fadeTimer()  {
-        console.log("Display: ")
-        var fadeEffect = setInterval(function () {
-            if (!display.style.opacity) {
-                display.style.opacity = 1;
-            }
-            if (display.style.opacity > 0) {
-                display.style.opacity -= 0.05;
-            } else {
-                clearInterval(fadeEffect);
-            }
-        }, 100);
-    }
-**/
-/**
-window.onclick = function showTimer ()  {
-        console.log("Display2: ")
-        //TODO fix damit man den Timer wieder einblenden kann
-        var showEffect = setInterval(function () {
-            if (!display.style.opacity) {
-                display.style.opacity = 1;
-            }
-            if (display.style.opacity < 1) {
-                display.style.opacity += 0.5;
-            } else {
-                clearInterval(showEffect);
-            }
-        }, 100);
-    }
-**/
